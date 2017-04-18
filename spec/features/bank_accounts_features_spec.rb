69ac465bcd8ef87446863a03a2a664f4e67794dd
Eva Balog <evebalog@netscape.net>
2017-04-18 14:23:53 +0100

    Added more feature tests

--- spec/features/bank_accounts_features_spec.rb
+++ spec/features/bank_accounts_features_spec.rb
@@ -6,4 +6,15 @@ feature 'bank accounts' do
       expect(page).to have_link 'Add a new bank account'
     end
   end
+    context 'bank accounts have been added' do
+    before do
+      BankAccount.create(name: 'Eva\'s account')
+    end
+
+    scenario 'display bank account' do
+      visit '/bank_accounts'
+      expect(page).to have_content('Eva\'s account')
+      expect(page).not_to have_content('No bank accounts yet')
+    end
+  end
 end
