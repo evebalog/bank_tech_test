5254392a8fae108f667942df7675774c92b36484
Eva Balog <evebalog@netscape.net>
2017-04-18 15:29:09 +0100

    Created html form for new action,updated controller

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -2,4 +2,19 @@ class BankAccountsController < ApplicationController
   def index
     @bank_accounts = BankAccount.all
   end
+
+  def new
+    @bank_account = BankAccount.new
+  end
+
+  def create
+    @bank_account = BankAccount.create(bank_account_params)
+    redirect_to '/bank_accounts'
+  end
+
+  private
+
+  def bank_account_params
+    params.require(:bank_account).permit(:name)
+  end
 end
--- spec/features/bank_accounts_features_spec.rb
+++ spec/features/bank_accounts_features_spec.rb
@@ -7,14 +7,22 @@ feature 'bank accounts' do
     end
   end
     context 'bank accounts have been added' do
-    before do
-      BankAccount.create(name: 'Eva\'s account')
-    end
+    before { BankAccount.create(name: 'Current account') }
 
     scenario 'display bank account' do
       visit '/bank_accounts'
-      expect(page).to have_content('Eva\'s account')
+      expect(page).to have_content('Current account')
       expect(page).not_to have_content('No bank accounts yet')
+    end
+  end
+  context 'creating bank accounts' do
+    scenario 'prompts user to fill out a form, then displays the new bank account' do
+      visit '/bank_accounts'
+      click_link 'Add a new bank account'
+      fill_in 'Name', with: 'Current account'
+      click_button 'Create bank account'
+      expect(page).to have_content 'Current account'
+      expect(current_path).to eq '/bank_accounts'
     end
   end
 end
