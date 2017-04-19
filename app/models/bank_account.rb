fb461d96e14dfa0b1f8d5ad45ce0d8fbc4fd0b36
Eva Balog <evebalog@netscape.net>
2017-04-19 23:09:09 +0100

    Retract logic from view by creating methods to calculate deposits and withdrawal totals

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,4 +1,11 @@
 class BankAccount < ActiveRecord::Base
   has_many :deposits, dependent: :destroy
   has_many :withdrawals, dependent: :destroy
+
+  def deposit_total
+    deposits.pluck(:amount).reduce(:+)
+  end
+  def withdrawal_total
+    withdrawals.pluck(:amount).reduce(:+)
+  end
 end
