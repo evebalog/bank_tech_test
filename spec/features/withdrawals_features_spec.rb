ae87143df15cdaaddb963458a7b18ccf85afbc36
Eva Balog <evebalog@netscape.net>
2017-04-19 16:39:24 +0100

    Added feature test for withdrawal

---
+++ spec/features/withdrawals_features_spec.rb
@@ -0,0 +1,15 @@
+feature 'withdrawals' do
+  before { BankAccount.create(name: 'Current account')}
+
+  scenario 'allows user to withdraw an amount using a form' do
+    visit '/bank_accounts'
+    click_link 'Make a deposit to Current account'
+    fill_in 'deposit_deposit_amount', with: '500'
+    click_button 'Confirm deposit'
+    click_link 'Withdraw from Current account'
+    fill_in 'Debit', with: '100'
+    click_button 'Confirm withdrawal'
+    expect(current_path).to eq '/bank_accounts'
+    expect(page).to have_content('400')
+  end
+end
