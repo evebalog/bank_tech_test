8623c15641a913a0b93b7172361b7cc4aee84909
Eva Balog <evebalog@netscape.net>
2017-04-20 10:54:14 +0100

    Rename fields in feature tests to pass

--- spec/features/withdrawals_features_spec.rb
+++ spec/features/withdrawals_features_spec.rb
@@ -4,10 +4,10 @@ feature 'withdrawals' do
   scenario 'allows user to withdraw an amount using a form' do
     visit '/bank_accounts'
     click_link 'Make a deposit to Current account'
-    fill_in 'amount', with: '500'
+    fill_in 'deposit_amount', with: '500'
     click_button 'Confirm deposit'
     click_link 'Withdraw from Current account'
-    fill_in 'Withdraw', with: '100'
+    fill_in 'withdrawal_amount', with: '100'
     click_button 'Confirm withdrawal'
     expect(current_path).to eq '/bank_accounts'
     expect(page).to have_content('400')
