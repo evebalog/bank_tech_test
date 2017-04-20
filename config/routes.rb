6a77cedd69a4a09434b157d05fc1839e70826580
Eva Balog <evebalog@netscape.net>
2017-04-20 12:58:43 +0100

    Add root for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :deposits
     resources :withdrawals
   end
+
+  root 'bank_accounts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
