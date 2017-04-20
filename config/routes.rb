8c71e2a58eea0e1cbc7b93f369386ffef0b25588
Eva Balog <evebalog@netscape.net>
2017-04-20 13:01:43 +0100

    Define homepage route for app

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :withdrawals
   end
 
-  root 'bank_accounts#index'
+  root 'bank_accounts#index', page: 'home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
