bf331f611d54f13613f9f2da38d78f3bc3f6a6e0
Eva Balog <evebalog@netscape.net>
2017-04-20 10:47:55 +0100

    Remove unused helper method

--- app/helpers/bank_accounts_helper.rb
+++ app/helpers/bank_accounts_helper.rb
@@ -1,10 +1,2 @@
 module BankAccountsHelper
-  def show_balance(bank_account)
-    @deposits = Deposit.where("bank_account_id = ?", bank_account.id)
-    @balance = []
-    @deposits.each do |deposit|
-      @balance << deposit.amount
-    end
-    @balance
-  end
 end
